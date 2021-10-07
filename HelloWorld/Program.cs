051131ee9f84160c6a22129c0287846d739e22e6
azace <azace@PX-ALVIS>
2021-10-07 20:44:27 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jelena");
+            Console.WriteLine("My name is Jelena.\n\nI was born and raised in Rezekne in Latgale Region of eastern Latvia.\nAfter finishing school I moved to Riga to stady at university.");
+            Console.WriteLine("Since school years I had tried different hobbies.");
+            Console.WriteLine("Some of them were very active such as cycling and dancing - I even participated in dance competitions.");
+            Console.WriteLine("Some of the hobbies were ver calm such as Bikram yoga.");
+            Console.WriteLine("Some of them were very creative like photography, make-up course and floral design.");
+            Console.WriteLine("Time has passed and my interests have changed as well.");
+            Console.WriteLine("What I really love are travelling and dogs.");
+            Console.WriteLine("I have visited 19 countries so far.");
+            Console.WriteLine("My farthest journey was Philippines in 2019.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
